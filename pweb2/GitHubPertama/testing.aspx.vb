f8adc9d89a863723557b098fbe16b7b75b94b5d7
WinnieAlison <winnie15ti@mahasiswa.pcr.ac.id>
2017-10-27 09:00:35 +0700

    Testing kedua

---
+++ pweb2/GitHubPertama/testing.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class testing
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Testing kesekian dari GitHub dan Visual Studio"
+    End Sub
+
+End Class
